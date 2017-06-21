49c25a04ba069554f0928c6208f4def9a21d9000
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-06-21 15:03:10 -0500

    add migration

---
+++ app/models/illusion.rb
@@ -0,0 +1,11 @@
+class Illusion < ApplicationRecord
+  belongs_to :creator, class_name: :Users
+  has_many :illusions_tags
+  has_many :tags, through: :illusions_tags
+  has_many :starrings
+  has_many :starrers, through: :starrings, source: :user
+
+
+  validates :title, :url, :creator_id, presence: true
+
+end
--- db/migrate/20170621193945_create_table_illusions.rb
+++ db/migrate/20170621193945_create_table_illusions.rb
@@ -1,7 +1,11 @@
 class CreateTableIllusions < ActiveRecord::Migration[5.1]
   def change
     create_table :table_illusions do |t|
-      t.
+      t.string :title, null: false
+      t.string :url, null: false
+      t.boolean :intense, default: false
+      t.integer :creator_id, null: false
+      t.timestamps(null: false)
     end
   end
 end
