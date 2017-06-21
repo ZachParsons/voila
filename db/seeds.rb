d9909b68a9e7600d8db0edcb7fab47f9a831c723
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-06-21 15:24:16 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+illusions = 10.times do
+  Illusion.create(title: Faker::HitchhikersGuideToTheGalaxy.specie,
+                  url: Faker::Internet.url,
+                  intense: Faker::Boolean.boolean,
+                  creator_id: Faker::Number.between(1, 6))
+end
