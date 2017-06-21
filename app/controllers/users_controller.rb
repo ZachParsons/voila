2612f354172fecc8284f1604a79f971c83c45972
Jack Howard & Kosha Patel <pair+jackhowa.kpatel7389@devbootcamp.com>
2017-06-21 15:07:19 -0500

    add a link_to get to the register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:stage_name, :email, :password)
   end
