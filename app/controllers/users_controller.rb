c50bbe97fd3051458911af949262ec766ec05cab
Jack Howard & Kosha Patel <pair+jackhowa.kpatel7389@devbootcamp.com>
2017-06-21 16:58:51 -0500

    able to use conditional setter, check for whether Frederiq is the secret key shush, if wrong key secret then an apprentice will still be made by default

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
+
+    if @user.check_master(params[:user][:secret])
+      @user.master = true
+    end
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user
@@ -16,10 +21,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-
-
   private
   def user_params
-    params.require(:user).permit(:stage_name, :email, :password)
+    params.require(:user).permit(:stage_name, :secret, :email, :password)
   end
 end
