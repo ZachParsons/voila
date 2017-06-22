d54746e5546a40d87de3780ee49f225664b9c6d1
jackhowa <johncarverhoward@gmail.com>
2017-06-22 08:22:58 -0500

    add a controller for the sessions. The controller has a create and delete for logging and logout. The layout has a login, logout, and register functionality and links. There's conditional logic for whether there's a session id present or not. If the session id is present, then the register button doesn't show and a logout button shows instead. We've talked about hiding the login and register button on the actual login and register view but not sure how this would work. https://trello.com/c/nzRuKfUW/2-as-a-logged-in-user-i-can-view-my-own-profile-page-and-see-own-resources-so-others-cant-snoop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users, :sessions
 
   get '/users/new/masters', to: 'users#new_master', as: :new_master
