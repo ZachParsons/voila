7cddacb073fe836207ba5d55c17954f8493ede13
Jack Howard & Zach Parsons <pair+jackhowa.zachparsons@devbootcamp.com>
2017-06-22 18:01:45 -0500

    add the illusions tags show page that work based on current user and model methods

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,23 @@
 class Tag < ApplicationRecord
   has_many :illusion_tags
   has_many :illusions, through: :illusion_tags
+
+  def tags_with_illusions_by_status(user)
+    all_tag_illusions = self.illusions
+    appropriate_intensity_illusions_by_tag = all_tag_illusions.where(intense: user.master)
+    appropriate_intensity_illusions_by_tag.count > 0
+  end
+
+  def self.find_appropriate_tags(user)
+    Tag.all.select do |tag|
+      tag.tags_with_illusions_by_status(user)
+    end
+  end
+
+  def self.find_appropriate_unlogged_in_tags
+    Tag.all.select do |tag|
+      tag_count = tag.illusions.where(intense: false).length
+      tag_count > 0
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, :sessions, :tags, :illusions
 
-  get '/illusions', to: 'illusions#index', as: 'masters_illusions'
+  # get '/illusions', to: 'illusions#index', as: 'masters_illusions'
 
   get '/users/new/masters', to: 'users#new_master', as: :new_master
 
