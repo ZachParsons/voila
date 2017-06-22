32a89f222e523de17d36d2c4856ec4e5b06e5028
Braidy Merkle & Zach Parsons <pair+bmerkle3.zachparsons@devbootcamp.com>
2017-06-21 20:31:47 -0500

    add masters-only illusions page

--- app/controllers/illusions_controller.rb
+++ app/controllers/illusions_controller.rb
@@ -1,16 +1,13 @@
 class IllusionsController < ApplicationController
-  # def index
-  # end
+  def index
+    @masters_illusions = Illusion.all.where(intense: true)
+  end
 
   def new
     @illusion = Illusion.new
   end
 
   def create
-    p "illusion_params #{illusion_params}"
-    # # "@illusion #{@illusion = Illusion.new(illusion_params)}"
-    # @illusion = Illusion.new(title: illusion_params.title, url: illusion_params.url, intense: illusion_params.intense, creator_id: session[:user_id])
-    # # "@illusion.creator_id #{@illusion.creator_id = session[:user_id]}"
     @illusion = Illusion.new(illusion_params)
     @illusion.creator_id = session[:user_id]
 
@@ -20,6 +17,9 @@ class IllusionsController < ApplicationController
       @errors = @illusion.errors.full_messages
       render :new, status: 422
     end
+  end
+
+  def show
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   resources :illusions
 
+  get '/illusions', to: 'illusions#index', as: 'masters_illusions'
+
+  # namespace :masters do
+  #   resource :illusions, controller: :illusions, only: [:index],
+  # end
+
   get '/users/new/masters', to: 'users#new_master', as: :new_master
 
 end
