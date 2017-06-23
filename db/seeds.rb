0c803e825c6c5a35633f8c194a16110244e9b87d
jackhowa <johncarverhoward@gmail.com>
2017-06-23 13:08:08 -0500

    add illusions of varying approval and user status

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,40 +8,35 @@ end
 
 masters = User.where(master: true)
 
-# 5.times do 
+15.times do 
+	apprentice = User.new(stage_name: Faker::LordOfTheRings.character + " " + Faker::Name.last_name, password: "password", master: false, a_master: masters.sample)
+	apprentice.email = Faker::Internet.email(apprentice.stage_name)
+	apprentice.save!
+end
 
-# end
+apprentices = User.where(master: false)
 
-# 8.times do
-#   User.create!(stage_name: Faker::Space.planet + " " + Faker::Space.moon, email: Faker::Internet.safe_email, password: "voila", master: true, master_id: nil)
-# end
+# approved master illusions 
+25.times do 
+	approved_master_illusion = Illusion.new(title: Faker::Hipster.word + " " + Faker::Space.nasa_space_craft + " " + ["trick","illusion","disappearing act","splitting", "sawing", "death"].sample, creator: masters.sample, approval: true, intense: [true, false].sample)
+	approved_master_illusion.url = Faker::Internet.url(approved_master_illusion.title)
+	approved_master_illusion.save!
+end 
+
+# unapproved apprentice illusions 
+15.times do 
+	unapproved_apprentice_illusion = Illusion.new(title: ["cute","super easy","arp","ARP","cool", "idiots guide to"].sample + " " + ["card","sea lion"].sample + " " + ["flip trick","arp","joke","unconvincing illusion"].sample, creator: apprentices.sample, approval: false, intense: false)
+	unapproved_apprentice_illusion.url = Faker::Internet.url(unapproved_apprentice_illusion.title)
+	unapproved_apprentice_illusion.save!
+end
+
+# approved apprentice unintense 
+10.times do 
+	approved_apprentice_illusion = Illusion.new(title: ["cute","super easy","lol","easy","fun"].sample + " " + ["card","bird","pigeon", "sea lion"].sample + " " + ["trick","illusion","trickery"].sample, creator: apprentices.sample, approval: true, intense: false)
+	approved_apprentice_illusion.url = Faker::Internet.url(approved_apprentice_illusion.title)
+	approved_apprentice_illusion.save!
+end
 
 
-
-
-
-
-
-
-
-
-
-
-
-# 8.times do
-#   User.create!(stage_name: Faker::Space.planet + " " + Faker::Space.moon, email: Faker::Internet.safe_email, password: "voila", master: true, master_id: nil)
-# end
-
-# 2.times do
-#   User.create!(stage_name: Faker::Space.planet + " " + Faker::Space.moon, email: Faker::Internet.safe_email, password: "voila", master_id: Faker::Number.between(1, 8))
-# end
-
-# users = User.all
-
-# illusions = 10.times.map do
-#   Illusion.create!(title: Faker::Ancient.titan,
-#                   url: Faker::Internet.url,
-#                   intense: Faker::Boolean.boolean,
-#                   creator: users.sample,
-#                   approval: Faker::Boolean.boolean)
-# end
+# apprentice stars 
+# 25.times 
