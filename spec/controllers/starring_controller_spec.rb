b851840489d422d65000650e6bc9ec8ab492e439
Zach Parsons <email": null>
2017-06-23 16:40:34 -0500

    add authorization wrapper to users#show

---
+++ spec/controllers/starring_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# describe StarringsController do
+
+  # let!(:star) {Starring.create!(illusion_id: 1, starrer_id: 1)}
+
+#   describe "post #create" do
+#     it "responds with status code 200" do
+#       :star = Starring.create(illusion_id: 1, starrer_id: 1)
+#       expect(assigns(:star)).to be_a(Star)
+#     end
+#   end
+# end
