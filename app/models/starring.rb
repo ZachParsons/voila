b8ef0a0528c5eaae93a24019f16c06d070644963
Zach Parsons <email": null>
2017-06-23 15:38:21 -0500

    satrring custom validation in progress

--- app/models/starring.rb
+++ app/models/starring.rb
@@ -1,4 +1,8 @@
 class Starring < ApplicationRecord
   belongs_to :illusion
   belongs_to :starrer, class_name: :User, foreign_key: :starrer_id
+
+  # validates :illusion, uniqueness: {scope: :starrer,
+  #   message: "Only able to star an illusion once."
+  # }
 end
